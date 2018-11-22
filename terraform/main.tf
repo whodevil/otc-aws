72f1c0c42fde34842edde0d283a275b68edbec61
whodevil <whodevil@offthecob.info>
2018-11-21 16:00:44 -0800

    Adding terraform

---
+++ terraform/main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "default" {
+  vpc_id = "${aws_vpc.default.id}"
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = "${aws_vpc.default.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.default.id}"
+}
+
+resource "aws_subnet" "default" {
+  vpc_id                  = "${aws_vpc.default.id}"
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_s3_bucket" "site" {
+  bucket = "site"
+  acl    = "private"
+
+  tags {
+    Name = "static site"
+  }
+}
+
+locals {
+  s3_origin_id = "myS3Origin"
+}
+
+
