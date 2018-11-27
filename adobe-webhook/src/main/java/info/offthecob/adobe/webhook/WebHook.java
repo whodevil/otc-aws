c10936a2e2e07e808f8628777ce0faf47c4c5690
whodevil <whodevil@offthecob.info>
2018-11-26 17:37:34 -0800

    ignore terraform staging dir

--- adobe-webhook/src/main/java/info/offthecob/adobe/webhook/WebHook.java
+++ adobe-webhook/src/main/java/info/offthecob/adobe/webhook/WebHook.java
@@ -5,12 +5,12 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class WebHook implements RequestHandler<String, Void> {
+public class WebHook implements RequestHandler<String, String> {
 
     @Override
-    public Void handleRequest(String input, Context context) {
+    public String handleRequest(String input, Context context) {
         LambdaLogger logger = context.getLogger();
         logger.log("working");
-        return null;
+        return "OK";
     }
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,4 @@
-provider "aws" {
-  region = "${var.aws_region}"
-}
+provider "aws" {}
 
 resource "aws_vpc" "default" {
   cidr_block = "10.0.0.0/16"
@@ -22,17 +20,46 @@ resource "aws_subnet" "default" {
   map_public_ip_on_launch = true
 }
 
+variable "bucket_name" {
+  default = "site"
+}
+
 resource "aws_s3_bucket" "site" {
-  bucket = "site"
-  acl    = "private"
+  bucket = "${var.bucket_name}"
+  acl    = "public-read"
 
   tags {
     Name = "static site"
   }
+
+  policy = <<EOF
+{
+  "Id": "bucket_policy_site",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "bucket_policy_site_main",
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::${var.bucket_name}/*",
+      "Principal": "*"
+    }
+  ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
 }
 
-locals {
-  s3_origin_id = "myS3Origin"
+output "website_domain" {
+  value = "${aws_s3_bucket.site.website_domain}"
 }
 
-
+output "website_endpoint" {
+  value = "${aws_s3_bucket.site.website_endpoint}"
+}
