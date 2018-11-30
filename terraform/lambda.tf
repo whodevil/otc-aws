e2a1716688c594f7f6cc8c1e1477002ed3a66a6e
whodevil <whodevil@offthecob.info>
2018-11-30 12:39:56 -0800

    fix up the lambda webhook logic in terraform

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -17,13 +17,32 @@ resource "aws_iam_role" "adobe_webhook_role" {
 EOF
 }
 
+resource "aws_iam_role_policy" "adobe_webhook_logging" {
+  role = "${aws_iam_role.adobe_webhook_role.name}"
+  name = "AdobeWebHookLoggingPolicy"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+       "Action": ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"],
+       "Effect": "Allow",
+       "Resource": ["*"]
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_lambda_function" "adobe_webhook" {
   function_name = "AdobeWebhook"
-  handler = "info.offthecob.adobe.webhook.Webhook::handleRequest"
+  handler = "info.offthecob.adobe.webhook.AdobeWebHook::handler"
   role = "${aws_iam_role.adobe_webhook_role.arn}"
   runtime = "java8"
+  timeout = 15
+  memory_size = 256
   description = "Handles adobe events for published images"
-  filename = "${path.module}/../adobe-webhook/build/distributions/adobe-webhook.zip"
+  filename = "${path.module}/../adobe-webhook/build/libs/adobe-webhook-all.jar"
 }
 
 resource "aws_lambda_permission" "adobe_webhook" {
@@ -33,5 +52,5 @@ resource "aws_lambda_permission" "adobe_webhook" {
   principal     = "apigateway.amazonaws.com"
 
   # https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "${aws_api_gateway_deployment.otc_api.execution_arn}/GET/webhook"
+  source_arn = "${aws_api_gateway_deployment.otc_api.execution_arn}/*"
 }
