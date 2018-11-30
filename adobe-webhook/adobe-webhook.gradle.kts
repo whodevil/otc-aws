f4c58cece2048716d84806cb3fb8d8160be91355
whodevil <whodevil@offthecob.info>
2018-11-30 12:36:23 -0800

    attempt at rewriting in kotlin

--- adobe-webhook/adobe-webhook.gradle.kts
+++ adobe-webhook/adobe-webhook.gradle.kts
@@ -1,18 +1,46 @@
+buildscript {
+    repositories {
+        mavenCentral()
+        jcenter()
+    }
 
-plugins{
-    `java`
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.10")
+        classpath("com.github.jengelman.gradle.plugins:shadow:+")
+    }
+}
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+plugins {
+    kotlin("jvm") version "1.3.10"
+    idea
+}
+
+// This is really annoying, I can't figure out how to get it to run in the
+// plugins block. I'll need to loop back on this later when I learn the DSL better.
+apply(plugin = "com.github.johnrengelman.shadow")
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 dependencies {
     implementation("com.amazonaws:aws-lambda-java-core:+")
     implementation("com.amazonaws:aws-lambda-java-events:+")
     implementation("com.amazonaws:aws-lambda-java-log4j2:+")
+    implementation("com.beust:klaxon:3.0.1")
+    compile(kotlin("reflect"))
+    compile(kotlin("stdlib-jdk8"))
+    compile(kotlin("stdlib"))
 }
 
-tasks.create<Zip>("buildZip") {
-    from(tasks["compileJava"])
-    from(tasks["processResources"])
-    into("lib"){
-        from(configurations["compileClasspath"])
+tasks {
+    "build" {
+        dependsOn("shadowJar")
     }
 }
