27f66f7824d1bf4763086528459801ccfabad030
whodevil <whodevil@offthecob.info>
2018-12-09 20:46:01 -0800

    trying to get auto-completion going

--- adobe_webhook/adobe_webhook.py
+++ adobe_webhook/adobe_webhook.py
@@ -4,6 +4,7 @@ import boto3
 def handler(event, context):
     print(event)
     sqs = boto3.resource('sqs')
+    """ :type: pyboto3.sqs """
     queue = sqs.get_queue_by_name(QueueName='ImageSyncJobQueue')
     print(queue.url)
     return {"message": "Hello, World!"}
