65a4d666eea234c6828f7bb8421182d05cba44d9
whodevil <whodevil@offthecob.info>
2018-12-12 11:50:19 -0800

    first material config and button

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,26 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <React.Fragment >
+            <CssBaseline />
+            <Button variant="contained" color="Primary">
+            hello world
+            </Button>
+        </ React.Fragment>
     );
   }
 }
