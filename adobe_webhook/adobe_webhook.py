74688ea352f7af664014690c0e682a6c02a272b6
whodevil <whodevil@offthecob.info>
2018-12-23 21:55:59 -0800

    debugging lambda stuff

--- adobe_webhook/adobe_webhook.py
+++ adobe_webhook/adobe_webhook.py
@@ -1,12 +1,21 @@
 import boto3
 import json
+import pprint
+import aws_lambda_logging
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 def handler(event, context):
-    print("event: " + json.dumps(event))
-    print("context: " + json.dumps(context))
+    aws_lambda_logging.setup(level="INFO")
+    logger.info("event: " + json.dumps(event))
+    pp = pprint.PrettyPrinter(indent=4)
+    logger.info("context")
+    logger.info(pp.pprint(context))
     sqs = boto3.resource("sqs")
     queue = sqs.get_queue_by_name(QueueName="ImageSyncJobQueue")
-    print(queue)
+    logger.info(queue)
     queue.send_message(MessageBody="testing testing 123")
     return {"message": "Hello, World!"}
--- image_sync/image_sync.py
+++ image_sync/image_sync.py
@@ -1,13 +1,27 @@
-# import boto3
+import boto3
 import uuid
 import json
+import pprint
+import aws_lambda_logging
+from boto3_type_annotations.s3 import Client as S3Client
+from boto3_type_annotations.dynamodb import Client as DynamodbClient
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 def handler(event, context):
-    print("event: " + json.dumps(event))
-    print("context: " + json.dumps(context))
-    # dynamodb = boto3.client('dynamodb')
-    # s3 = boto3.client('s3')
+    aws_lambda_logging.setup(level="INFO")
+    logger.info("event: " + json.dumps(event))
+    pp = pprint.PrettyPrinter(indent=4)
+    logger.info(pp.pprint(context))
+    dynamodb: DynamodbClient = boto3.client("dynamodb")
+    s3: S3Client = boto3.client("s3")
     s3key = uuid.uuid4()
-    print("s3key: " + s3key)
+    logger.info("buckets")
+    logger.info(pp.pprint(s3.list_buckets()))
+    logger.info("dynamodb")
+    logger.info(pp.pprint(dynamodb.list_tables()))
+    logger.info("s3key: " + s3key)
     return {"message": "Hello, World!"}
