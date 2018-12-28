36982c9cc08823917b5ce8cbf92eff83fd78be42
whodevil <whodevil@offthecob.info>
2018-12-28 14:27:44 -0800

    hook up auth to the react app

--- otc-ui/src/App.js
+++ otc-ui/src/App.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+Amplify.configure(aws_exports);
 
 class App extends Component {
     render() {
@@ -15,4 +19,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default withAuthenticator(App, {includeGreetings: true});
